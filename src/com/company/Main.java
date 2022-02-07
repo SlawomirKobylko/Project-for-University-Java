d0b4c8577e33baf13433a5b644b9ac21564d95ad
slawektoja <kobylko996@o2.pl>
2022-02-07 15:48:51 +0100

    lsowanie poziomu projektu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,21 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        //System.out.println();
-        Projekty proj = new Projekty
-                (3, "ZUS", "comarch",
-                        "23.11.2022", 10000, 250000,"12.12.2022");
-
         Random ran = new Random();
         int x = ran.nextInt(3)*1;
 
-        System.out.println(x);
+        System.out.println("Zaczynamy grę!");
+        if (x==0)
+            System.out.println("Wylosowałeś prosty projekt.");
+        else if (x==1)
+            System.out.println("Wylosowałeś średni projekt.");
+        else{
+            System.out.println("Wylosowałeś złożony projekt. Do złożonych projektów potrzeba pracowników");
+            System.out.println("Czy posiadasz pracowników?");
+            /*if (pracownicy==0)
+            System.out.println("Nie posiadasz pracowników, musisz zacząć grę od nowa");
+            */
+        }
+
     }
 }
